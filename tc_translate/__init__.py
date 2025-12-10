7f864b13a5c29cbc1d0860897abbdc9b0c4bdaf8
Mich-Seth Owusu <michsethowusu@gmail.com>
2025-12-10 12:51:08 +0100

    Refactor TC Translator module and update version

---
+++ tc_translate/__init__.py
@@ -0,0 +1,9 @@
+"""
+TC Translator - Terminology Controlled Translator
+"""
+
+from .translator import TCTranslator, Translator
+from .terminology_manager import TerminologyManager
+
+__version__ = "0.1.0"
+__all__ = ['TCTranslator', 'Translator', 'TerminologyManager']
